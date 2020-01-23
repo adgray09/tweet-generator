d797b7bc2def284dfdf47db8f1923991d539326b
Razor <alexandergray37@gamil.com>
2020-01-23 11:43:00 -0800

    updated rearrange module

--- rearrange.py
+++ rearrange.py
@@ -1,11 +1,15 @@
 import random
 import sys as sys
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
+args = sys.argv[1:]
+    
+def rearrange():
     
     for rand_arg in args:
         randomize = random.randint(0, len(args)-1)
         random_word = args[randomize]
         print(random_word)
+    return rand_arg
+        
+rearrange()
 
