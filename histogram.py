807314a45c7ade518471c43bcd34bed4ca428e39
Razor <alexandergray37@gamil.com>
2020-01-24 21:20:23 -0800

    small error to be fixed

--- histogram.py
+++ histogram.py
@@ -1,14 +1,13 @@
 import sys
 
-words_list = ("one fish two fish red fish blue fish")
-words_list.split()
-
+with open('tintern_abbey.txt') as f:
+    my_list = [line.rstrip('\n') for line in f for word in line.split()]
+    
 def dictionary(words_string):
     hist = {}
-    words_list = words_string.split()
-    for i in words_list:
+    for i in my_list:
         hist[i] = hist.get(i, 0) + 1
     return hist
         
-counted = dictionary(words_list)
-
+counted = dictionary(my_list)
+print(counted)
